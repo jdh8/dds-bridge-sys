40ad236ec3f76cc430a241fd5621ebbb34a6f962
Chen-Pang He <chen.pang.he@jdh8.org>
2024-05-25 03:31:27 +0800

    Link to pre-installed dds for now

---
+++ build.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo:rustc-link-lib=dds");
+    let out_dir = PathBuf::from(env::var("OUT_DIR").expect("Cargo should have provided OUT_DIR"));
+
+    bindgen::Builder::default()
+        .header("vendor/src/dds.h")
+        .allowlist_file("vendor/.*")
+        .clang_arg("-xc++")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("Failed to generate bindings")
+        .write_to_file(out_dir.join("bindings.rs"))
+        .expect("Failed to write bindings");
+}
