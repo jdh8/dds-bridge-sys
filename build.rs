1f72469651216ea352c6f8bc0805d79494dada53
Chen-Pang He <chen.pang.he@jdh8.org>
2024-05-25 23:06:55 +0800

    Fix linking order

--- build.rs
+++ build.rs
@@ -3,11 +3,10 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() -> anyhow::Result<()> {
-    cc::Build::new()
-        .cpp(true)
-        .files(glob("vendor/src/*.cpp")?.flatten())
-        .define("DDS_THREADS_STL", None)
-        .compile("dds");
+    // This line must precede the cc::Build::try_compile call to ensure correct
+    // linking order.
+    println!("cargo:rustc-link-lib=dds");
+
     bindgen::Builder::default()
         .header("vendor/src/dds.h")
         .use_core()
@@ -16,13 +15,12 @@ fn main() -> anyhow::Result<()> {
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()?
         .write_to_file(PathBuf::from(env::var("OUT_DIR")?).join("bindings.rs"))?;
-    println!("cargo:rustc-link-lib=dds");
 
-    #[cfg(any(target_os = "macos", target_os = "ios", target_os = "freebsd", target_os = "dragonfly", target_os = "netbsd", target_os = "openbsd"))]
-    println!("cargo:rustc-link-lib=c++");
-    #[cfg(any(target_os = "linux", target_os = "none"))]
-    println!("cargo:rustc-link-lib=stdc++");
-    #[cfg(target_os = "android")]
-    println!("cargo:rustc-link-lib=c++_shared");
+    cc::Build::new()
+        .cpp(true)
+        .files(glob("vendor/src/*.cpp")?.flatten())
+        .define("DDS_THREADS_STL", None)
+        .try_compile("dds")?;
+
     Ok(())
 }
