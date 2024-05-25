29182dc6313eb60cfc5b2bd131c2b5d730528dd2
Chen-Pang He <chen.pang.he@jdh8.org>
2024-05-25 15:21:50 +0800

    Simplify error handling with anyhow

--- build.rs
+++ build.rs
@@ -1,18 +1,15 @@
 use std::env;
 use std::path::PathBuf;
 
-fn main() {
-    println!("cargo:rustc-link-lib=dds");
-    let out_dir = PathBuf::from(env::var("OUT_DIR").expect("Cargo should have provided OUT_DIR"));
-
+fn main() -> anyhow::Result<()> {
     bindgen::Builder::default()
         .header("vendor/src/dds.h")
         .use_core()
         .allowlist_file("vendor/.*")
         .clang_arg("-xc++")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        .generate()
-        .expect("Failed to generate bindings")
-        .write_to_file(out_dir.join("bindings.rs"))
-        .expect("Failed to write bindings");
+        .generate()?
+        .write_to_file(PathBuf::from(env::var("OUT_DIR")?).join("bindings.rs"))?;
+    println!("cargo:rustc-link-lib=dds");
+    Ok(())
 }
