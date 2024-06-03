01a9269b0c0ad2785be340d57727bd8573e6c8fe
Chen-Pang He <chen.pang.he@jdh8.org>
2024-06-04 00:07:43 +0800

    Remove platform-specific code after profiling

--- build.rs
+++ build.rs
@@ -15,35 +15,13 @@ fn main() -> anyhow::Result<()> {
         .generate()?
         .write_to_file(PathBuf::from(env::var("OUT_DIR")?).join("bindings.rs"))?;
 
-    let mut build = cc::Build::new();
-    build
+    cc::Build::new()
         .cpp(true)
         .files(glob("vendor/src/*.cpp")?.flatten())
         .std("c++14")
         .define("DDS_THREADS_STL", None)
-        .cargo_warnings(false);
-
-    #[cfg(windows)]
-    build.define("DDS_THREADS_WINAPI", None);
-
-    #[cfg(any(target_os = "macos", target_os = "ios"))]
-    build.define("DDS_THREADS_GCD", None);
-
-    #[cfg(feature = "openmp")]
-    env::var("DEP_OPENMP_FLAG")?.split(' ').for_each(|flag| {
-        build.flag_if_supported(flag);
-    });
-
-    build.try_compile("dds")?;
-
-    #[cfg(feature = "openmp")]
-    if let Some(link) = env::var_os("DEP_OPENMP_CARGO_LINK_INSTRUCTIONS") {
-        if !link.is_empty() {
-            for i in env::split_paths(&link) {
-                println!("cargo:{}", i.display());
-            }
-        }
-    }
+        .cargo_warnings(false)
+        .try_compile("dds")?;
 
     Ok(())
 }
