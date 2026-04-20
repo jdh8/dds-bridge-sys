1d4021cddf145b41ef20ec1ffebb18d4aed6ba1e
Chen-Pang He <chen.pang.he@jdh8.org>
2026-04-21 05:12:30 +0800

    Print thread sizes in the example

--- examples/info.rs
+++ examples/info.rs
@@ -1,3 +1,4 @@
+use core::ffi::CStr;
 use core::mem::MaybeUninit;
 use dds_bridge_sys as dds;
 
@@ -8,6 +9,12 @@ fn main() {
         dds::GetDDSInfo(info.as_mut_ptr());
         info.assume_init()
     };
+
     println!("DDS Version: {}.{}.{}", info.major, info.minor, info.patch);
     println!("Number of threads: {}", info.noOfThreads);
+
+    println!(
+        "Thread sizes: {}",
+        unsafe { CStr::from_ptr(info.threadSizes.as_ptr()) }.to_string_lossy()
+    );
 }
