cb2f92e7028a745ec131cf3aa58bea162c406a2b
Chen-Pang He <chen.pang.he@jdh8.org>
2026-04-24 16:55:15 +0800

    Do not pedantically lint generated code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![doc = include_str!("../README.md")]
 #![cfg_attr(not(test), no_std)]
-#![allow(non_upper_case_globals, non_camel_case_types, non_snake_case)]
+#![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, clippy::pedantic)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
