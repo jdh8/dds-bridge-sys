7734afd265564e35fa8ae511df08aa1ce70164d5
Chen-Pang He <chen.pang.he@jdh8.org>
2026-05-20 19:49:42 +0800

    Drop redundant naming-lint allows
    Upstream v3.0.0 uses PascalCase types and SCREAMING_SNAKE_CASE
    constants, so `non_camel_case_types` and `non_upper_case_globals`
    no longer fire. Function names like `SolveBoard` still need
    `non_snake_case`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![doc = include_str!("../README.md")]
 #![cfg_attr(not(test), no_std)]
-#![allow(non_upper_case_globals, non_camel_case_types, non_snake_case)]
+#![allow(non_snake_case)]
 #![allow(clippy::all, clippy::pedantic)]
 #![allow(rustdoc::broken_intra_doc_links)]
 
