594e715cec6b73a53f93d4c12cff3b9cb823b3ec
Chen-Pang He <chen.pang.he@jdh8.org>
2026-05-20 19:48:13 +0800

    Allow broken intra-doc links in bindings
    Bindgen translates upstream doxygen comments like `rho[0]=3` and
    `partner[i]` into Rust doc attributes, and rustdoc parses the
    brackets as intra-doc links. CI runs `cargo doc` with
    `RUSTDOCFLAGS=-Dwarnings`, so those 15 warnings fail the docs job.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 #![cfg_attr(not(test), no_std)]
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case)]
 #![allow(clippy::all, clippy::pedantic)]
+#![allow(rustdoc::broken_intra_doc_links)]
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
